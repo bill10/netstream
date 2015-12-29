87e2c7407d09940c75b7d4cba6c6ba5bd30932bf
bill10 <bill@Bills-MacBook-Pro.local>
2015-12-29 00:24:07 -0600

    protocol for graph change
    1. add node
    2. add edge
    
    A example of forming a triangle

--- server.py
+++ server.py
@@ -4,9 +4,6 @@ import json
 from collections import deque
 
 queue=deque();
-queue.append({'an': {'id': 'n0', 'label': 'n0', 'x': 0, 'y': 0, 'size': 1}})
-queue.append({'an': {'id': 'n1', 'label': 'n1', 'x': 0, 'y': 1, 'size': 1}})
-queue.append({'an': {'id': 'n2', 'label': 'n2', 'x': 1, 'y': 1, 'size': 1}})
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'secret!'
@@ -15,6 +12,12 @@ socketio = SocketIO(app)
 @socketio.on('connect')
 def handle_connect():
     print("User connected!")
+    queue.append({'an': {'id': 'n0', 'label': 'n0', 'x': 0, 'y': 0, 'size': 1}})
+    queue.append({'an': {'id': 'n1', 'label': 'n1', 'x': 0, 'y': 1, 'size': 1}})
+    queue.append({'ae': {'id': 'e1', 'source': 'n1', 'target': 'n0'}})
+    queue.append({'an': {'id': 'n2', 'label': 'n2', 'x': 1, 'y': 1, 'size': 1}})
+    queue.append({'ae': {'id': 'e2', 'source': 'n1', 'target': 'n2'}})
+    queue.append({'ae': {'id': 'e3', 'source': 'n0', 'target': 'n2'}})
 
 @socketio.on('GET')
 def handle_message(message):
