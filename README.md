bc1adedd8b1d45954169c3366cb6c38170cb7c0b
bill10 <bill@wireles-guest-16-43-113.uchicago.edu>
2015-12-29 18:40:51 -0600

    version 1.0
    1. Defined event protocols
    2. Able to visualize dynamic graphs

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # netstream
 Visualization of streaming network data
+
+
+Add a node:
+{'an': {'id': str, 'label': str, 'x': num, 'y': num, 'size': num, 'type': ...}}  
+
+Add an edge between existing nodes:
+{'ae': {'source': node id, 'target': node id}}  
+
+Add an edge with one new node:  
+{'aen': {'new': {'id': str, 'label': str, 'x': num, 'y': num, 'size': num, 'type': ...},   
+         'old': node id,
+         'source': node id, 'target': node id}}  
+
+Add an edge with two new nodes:  
+{'aenn': {'source': {'id': str, 'label': str, 'x': num, 'y': num, 'size': num, 'type': ...},   
+          'target': {'id': str, 'label': str, 'x': num, 'y': num, 'size': num, 'type': ...}}  
--- server.py
+++ server.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import json
 from collections import deque
+import pandas as pd
+import random
 
 queue=deque();
+df=pd.read_csv('data/AllYearsReport.csv')
+df.loc[df['Organization Name'].isnull(),'Organization Name']="Unknown"
 
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'secret!'
@@ -12,12 +16,39 @@ socketio = SocketIO(app)
 @socketio.on('connect')
 def handle_connect():
     print("User connected!")
-    queue.append({'an': {'id': 'n0', 'label': 'n0', 'x': 0, 'y': 0, 'size': 1}})
-    queue.append({'an': {'id': 'n1', 'label': 'n1', 'x': 0, 'y': 1, 'size': 1}})
-    queue.append({'ae': {'id': 'e1', 'source': 'n1', 'target': 'n0'}})
-    queue.append({'an': {'id': 'n2', 'label': 'n2', 'x': 1, 'y': 1, 'size': 1}})
-    queue.append({'ae': {'id': 'e2', 'source': 'n1', 'target': 'n2'}})
-    queue.append({'ae': {'id': 'e3', 'source': 'n0', 'target': 'n2'}})
+    queue.clear()
+    node2id={}
+    edges=set()
+    N=0
+    E=0
+    for t in sorted(df['Year'].unique()):
+        df2=df[df['Year']==t]
+        for i in xrange(df2.shape[0]):
+            s=df2.iloc[i]['Contact PI / Project Leader']
+            t=df2.iloc[i]['Organization Name']
+            if s not in node2id:
+                N+=1
+                node2id[s]=str(N)
+                if t not in node2id:
+                    N+=1
+                    node2id[t]=str(N)
+                    queue.append({'aenn': {'source': {'id': node2id[s], 'label': s, 'size': 1, 'color': '#43a2ca'},
+                                           'target': {'id': node2id[t], 'label': t, 'size': 1, 'color': '#C32148'}}})
+                else:
+                    queue.append({'aen': {'new': {'id': node2id[s], 'label': s, 'size': 1, 'color': '#43a2ca'},
+                                          'old': node2id[t],
+                                          'source': node2id[s], 'target': node2id[t]}})
+                edges.add(node2id[s]+'_'+node2id[t])
+            else:
+                if t not in node2id:
+                    N+=1
+                    node2id[t]=str(N)
+                    queue.append({'aen': {'new': {'id': node2id[t], 'label': t, 'size': 1, 'color': '#C32148'},
+                                          'old': node2id[s],
+                                          'source': node2id[s], 'target': node2id[t]}})
+                    edges.add(node2id[s]+'_'+node2id[t])
+                else:
+                    queue.append({'ae': {'source': node2id[s], 'target': node2id[t]}})
 
 @socketio.on('GET')
 def handle_message(message):
